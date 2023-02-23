69545acafabd0ec8d925746c092c3afec7552231
João <joaoaugusto311016@gmail.com>
2023-02-23 15:22:32 -0300

    Atualizei

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import Input from "./components/titulo/input/input"
 import Lista from "./components/titulo/lista/lista"
 import Titulo from "./components/titulo/titulo"
-
 
 function App() {
 
   return (
     <>
-      <div className="Container__controler">
-        <Titulo />
-        <Input />
-        <Lista />
-      </div>
+      <Titulo />
+      <Lista />
     </>
   )
 }
--- src/components/titulo/lista/lista.jsx
+++ src/components/titulo/lista/lista.jsx
@@ -1,32 +1,51 @@
-
-import "./styles.css"
+import { useState } from "react"
+import "./styles.css";
 
 
 const Lista = () => {
-    return (
-        <div className="Container container__Lista">
-            <ul>
-                <li className="Container__caixa">
-                    <div className="Container__conteudo">
-                        <p className="descricao">Estudar Programação</p>
-                        <div>
-                            <a className="img"><i class='bx bx-check-double'></i></a>
-                            <a><i class='bx bxs-trash-alt'></i></a>
-                        </div>
-                    </div>
-                </li>
 
-                <li className="Container__caixa">
-                    <div className="Container__conteudo">
-                        <p className="descricao">Estudar Programação</p>
-                        <div>
-                            <a className="img"><i class='bx bx-check-double'></i></a>
-                            <a><i class='bx bxs-trash-alt'></i></a>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-        </div>
+    const [inputValue, setInputValue] = useState("");
+    const [list, setList] = useState([]);
+
+    const pegaInputValue = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const pegaClick = () => {
+        const newItem = { text: inputValue };
+        setList([...list, newItem]);
+        setInputValue('');
+    };
+
+
+    console.log(inputValue);
+
+
+    return (
+        <div>
+            <div className="Container container_input">
+                <input className="input" type={Text} placeholder="Digite sua tarefa" onChange={pegaInputValue}></input>
+                <button onClick={pegaClick}>Add</button>
+            </div>
+
+            <div className="Container container__Lista">
+                <ul>
+                    {list.map((item, index) => (
+                        <li className="Container__caixa" key={index}>
+                            <div className="Container__conteudo">
+                                <p className="descricao">{item.text}</p>
+                                <div>
+                                    <a><i class='bx bx-check-double'></i></a>
+                                    <a><i class='bx bxs-trash-alt'></i></a>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+
+                </ul >
+
+            </div >
+        </div >
     )
 }
 
