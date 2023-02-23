1b3b0d3aa337a18212e1b301946e4d4b0a9001be
João <joaoaugusto311016@gmail.com>
2023-02-23 19:24:49 -0300

    Atualizei

--- src/components/titulo/lista/lista.jsx
+++ src/components/titulo/lista/lista.jsx
@@ -16,26 +16,31 @@ const Lista = () => {
         };
 
         setList([...list, newInput]);
+        setInputValue('');
     }
 
+    const removeInput = (id) => {
+        const newList = list.filter(inputValue => inputValue.id !== id);
+        setList(newList);
+    }
 
 
     return (
         <div>
             <div className="Container container_input">
-                <input className="input" type={Text} placeholder="Digite sua tarefa" onChange={(e) => setInputValue(e.target.value)}></input>
+                <input value={inputValue} className="input" type={Text} placeholder="Digite sua tarefa" onChange={(e) => setInputValue(e.target.value)}></input>
                 <button onClick={addInput}>Add</button>
             </div>
 
             <div className="Container container__Lista">
                 <ul>
                     {list.map((inputValue) => (
-                        <li id="id" className="Container__caixa" key={inputValue.id}>
+                        <li className="Container__caixa" key={inputValue.id}>
                             <div className="Container__conteudo">
-                                <p className="descricao">{inputValue.inputValue}</p>
+                                <p checked="true" className="descricao ">{inputValue.inputValue}</p>
                                 <div>
-                                    <a><i class='bx bx-check-double'></i></a>
-                                    <a><i class='bx bxs-trash-alt'></i></a>
+                                    <a><i onClick={() => certo(inputValue.id)} class='bx bx-check-double'></i></a>
+                                    <a><i onClick={() => removeInput(inputValue.id)} class='bx bxs-trash-alt'></i></a>
                                 </div>
                             </div>
                         </li>
